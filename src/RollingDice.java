1638ab67105eacd0dc332d658d500678a3c3bbf8
DonPhister <s165233@student.dtu.dk>
2016-09-27 21:17:12 +0200

    sum of dice now working

--- src/RollingDice.java
+++ src/RollingDice.java
@@ -9,13 +9,15 @@ public class RollingDice {
 		die1 = new Die();
 		die2 = new Die();
 		
-		die1.roll();
-		die2.roll();
-		System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
 		
 		die1.roll();
 		die2.setFaceValue(4);
-
-	}
-
+		
+		// Prints out value of both dice and the sum of the two
+		
+		System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
+		 sum = die1.getFaceValue() + die2.getFaceValue();
+		 System.out.println ("Sum: " + sum);
+		 
+		 } 
 }
